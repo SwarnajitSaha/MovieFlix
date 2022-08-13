cfade44b816dd75df2b66730258b06d42e58f9d9
Swarnajit <SWARNAJITSAHA68@GMAIL.COM>
2022-08-13 13:34:57 +0600

    Add project files.

---
+++ MovieFlix/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using MovieFlix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieFlix.Controllers
+{
+    public class HomeController : Controller
+    {
+        MovieFlixEntities db = new MovieFlixEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+           
+            string sql = "select * from MovieList";
+            List<MovieList> movielist = db.MovieLists.SqlQuery(sql).ToList();
+            ViewBag.movielist = movielist;
+            return View();
+        }
+    }
+}
---
+++ MovieFlix/Controllers/MovieDetailesController.cs
@@ -0,0 +1,32 @@
+﻿using MovieFlix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieFlix.Controllers
+{
+    public class MovieDetailesController : Controller
+    {
+        // GET: MovieDetailes
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detailes()
+        {
+
+            return Content("1st call");
+           
+
+        }
+        [HttpPost]
+        public ActionResult Detailes([Bind(Include= "movieID")] MovieList id)
+        {
+            var movieId = id.movieID;
+            ViewBag.movieId = movieId;
+            return View();
+        }
+    }
+}
